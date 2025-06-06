8bd2fdfc55f7eebd5c361434543b633a4a2bd192
per1Peteia <justusmanthey@googlemail.com>
2025-06-06 11:02:13 +0200

    middleware

---
+++ src/api/readiness.ts
@@ -0,0 +1,7 @@
+import { Response, Request } from "express"
+
+export async function handlerReadiness(_: Request, res: Response): Promise<void> {
+	res.set("Content-Type", "text/plain; charset=utf-8");
+	res.send("OK");
+	res.end();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import path from "path";
+import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./middleware.js";
 
 const app = express();
 const port = 8080;
 
-app.use("/", express.static(path.join(process.cwd(), "src")));
+app.use(middlewareLogResponses);
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
 	console.log(`listening on localhost:${port}`);
 });
 
