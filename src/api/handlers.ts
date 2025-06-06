dcdc4b69fd3a6c647bd47a6c5a21e330ecf9bf73
per1Peteia <justusmanthey@googlemail.com>
2025-06-06 15:59:03 +0200

    refactor: admin endpoints, POST reset

--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -7,9 +7,15 @@ export async function handlerReadiness(_: Request, res: Response): Promise<void>
 	res.end();
 }
 
-export async function handlerMetrics(_: Request, res: Response): Promise<void> {
-	res.set("Content-Type", "text/plain; charset=utf-8");
-	res.send(`Hits: ${config.fileServerHits}\n`);
+export async function handlerMetrics(_: Request, res: Response) {
+	res.set("Content-Type", "text/html; charset=utf-8");
+	res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileServerHits} times!</p>
+  </body>
+</html>
+`);
 	res.end();
 }
 
--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ const port = 8080;
 
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
-app.get("/healthz", handlerReadiness);
-app.get("/metrics", handlerMetrics);
-app.get("/reset", handlerReset);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.post("/admin/reset", handlerReset);
 
 app.listen(port, () => {
 	console.log(`listening on localhost:${port}`);
